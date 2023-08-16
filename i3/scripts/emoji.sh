5bc48f4ce498f9fb507bbccd2ab7c00412a74fa2
Wihy <105944317+itsWihy@users.noreply.github.com>
2023-08-16 07:32:48 +0300

    Plagarism
    Nice

--- i3/scripts/emoji.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cat ~/.config/i3/scripts/emoji.txt |rofi -dmenu -i -p "Which emoji? ⭐ " -theme /$HOME/.config/rofi/minimal.rasi -font "twemoji 16" | awk '{print $1}' | tr -d '\n' | xclip -selection cli
